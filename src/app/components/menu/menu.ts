1bb8e4470327ea1d6644731d28385fc34460be0e
GideaoBitencourt <victorgideao01@gmail.com>
2026-08-31 23:06:40 -0300

    pagina home criada

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
+  
+  mostrarElemento = false;
 
+  mostrar(): void {
+    this.mostrarElemento = !this.mostrarElemento;
+  }
 }
